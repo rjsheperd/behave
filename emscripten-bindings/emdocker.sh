9213d37dfc1a462652662581c14a2e77ed6c2141
Thomas DeVera <tomdev@centurylink.net>
2022-02-17 11:31:18 -0800

    add emscripten sdk project volume to emdocker.sh

--- emscripten-bindings/emdocker.sh
+++ emscripten-bindings/emdocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it  --rm   -v $(pwd):/src   -u $(id -u):$(id -g)   emscripten/emsdk
+docker run -it  --rm   -v $(pwd):/src -v /home/tdog/workspace/projects/git_projects/emscripten:/emscripten   -u $(id -u):$(id -g)   emscripten/emsdk
